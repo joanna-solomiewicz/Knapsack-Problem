8af89ef84aaea58bb8d65e3989a66d0d59684abb
Marcin Piniarski <piniarski.m@gmail.com>
2015-11-04 22:53:56 +0100

    Added 'comp' method to Object - enables sorting.

--- Object.cpp
+++ Object.cpp
@@ -13,3 +13,9 @@ Object::Object(int ordinal, int value, int weight) {
 	this->value = value;
 	this->weight = weight;
 }
+
+bool Object::comp(const Object *object1, const Object *object2) {
+	float ratio1 = (float)object1->value/object1->weight;
+	float ratio2 = (float)object2->value/object2->weight;
+	return (ratio1 < ratio2);
+}
--- Object.h
+++ Object.h
@@ -2,11 +2,14 @@
 
 class Object {
 public:
-	Object(int ordinal);
-	Object(int ordinal, int value, int weight);
-
     int ordinal;
     int value;
     int weight;
 
+public:
+	Object(int ordinal);
+	Object(int ordinal, int value, int weight);
+
+    static bool comp(const Object* object1, const Object* object2); // use like this: list<Object*>.sort(comp), created to enable sorting on pointers with list<Object*>.sort()
 };
+
